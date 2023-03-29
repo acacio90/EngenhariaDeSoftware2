c08f9b49a92a758feaa88c92ed3fc0c88cbf6a11
acacio90 <pedrorodrigues.2019@alunos.utfpr.edu.br>
2023-03-28 23:57:58 -0600

    Definindo classes

--- psp0/src/main/java/psp0/App.java
+++ psp0/src/main/java/psp0/App.java
@@ -1,9 +1,5 @@
 package psp0;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ psp0/src/main/java/psp0/LinkedList.java
@@ -0,0 +1,5 @@
+package psp0;
+
+public class LinkedList {
+    
+}
