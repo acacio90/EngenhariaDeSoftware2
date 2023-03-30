a042703194c10101a0ca1df9bcdfa0f3a40af165
acacio90 <pedrorodrigues.2019@alunos.utfpr.edu.br>
2023-03-30 13:13:49 -0300

    Atualizando testes

--- psp0/src/main/java/psp0/App.java
+++ psp0/src/main/java/psp0/App.java
@@ -2,18 +2,42 @@ package psp0;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        LinkedList horas = new LinkedList();
+        LinkedList proxy = new LinkedList();
 
-        list.insert(1);
-        list.insert(2.5);
-        list.insert(3);
-        list.insert(4.2);
-        list.insert(5);
+        horas.insert(160);
+        horas.insert(591);
+        horas.insert(114);
+        horas.insert(229);
+        horas.insert(230);
+        horas.insert(270);
+        horas.insert(128);
+        horas.insert(1657);
+        horas.insert(624);
+        horas.insert(1503);
 
-        list.printList();
+        proxy.insert(15.0);
+        proxy.insert(69.9);
+        proxy.insert(6.5);
+        proxy.insert(22.4);
+        proxy.insert(28.4);
+        proxy.insert(65.9);
+        proxy.insert(19.4);
+        proxy.insert(198.7);
+        proxy.insert(38.8);
+        proxy.insert(138.2);
 
-        double media = Calculator.calcularMedia(list);
-        double desvioPadrao = Calculator.calcularDesvioPadrao(list);
+        horas.printList();
+        proxy.printList();
+
+        double media = Calculator.calcularMedia(horas);
+        double desvioPadrao = Calculator.calcularDesvioPadrao(horas);
+
+        System.out.println("Média: " + media);
+        System.out.println("Desvio padrão amostral: " + desvioPadrao);
+
+        media = Calculator.calcularMedia(proxy);
+        desvioPadrao = Calculator.calcularDesvioPadrao(proxy);
 
         System.out.println("Média: " + media);
         System.out.println("Desvio padrão amostral: " + desvioPadrao);
