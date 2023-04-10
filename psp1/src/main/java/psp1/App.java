d4cf1d49751386b5f8e991965452973c27f04212
acacio90 <pedrorodrigues.2019@alunos.utfpr.edu.br>
2023-04-10 11:03:17 -0300

    Contando para cada método

--- psp1/src/main/java/psp1/App.java
+++ psp1/src/main/java/psp1/App.java
@@ -1,12 +1,17 @@
 package psp1;
 
 import java.io.*;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws IOException {
         Counter locCounter = new Counter();
-        File file = new File("/home/acacio/Desktop/EngenhariaDeSoftware2/psp0/src/main/java/psp0/App.java");
+        File file = new File("/home/acacio/Desktop/EngenhariaDeSoftware2/psp0/src/main/java/psp0");
         locCounter.countLOC(file);
         locCounter.printResults();
+        System.out.println("Total Lines of Code per Method:");
+        for (Map.Entry<String, Integer> entry : locCounter.getMethodLineCounts().entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
--- psp1/src/main/java/psp1/Counter.java
+++ psp1/src/main/java/psp1/Counter.java
@@ -1,6 +1,8 @@
 package psp1; 
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Counter {
 
@@ -9,25 +11,31 @@ public class Counter {
     private int methodCount = 0;
     private int classLines = 0;
     private int methodLines = 0;
+    private String currentMethod = null;
+    private Map<String, Integer> methodLineCounts = new HashMap<>();
 
     public int getTotalLines() {
         return totalLines;
     }
-    
+
     public int getClassCount() {
         return classCount;
     }
-    
+
     public int getMethodCount() {
         return methodCount;
     }
-    
+
     public int getClassLines() {
         return classLines;
     }
-    
+
     public int getMethodLines() {
         return methodLines;
+    }
+
+    public Map<String, Integer> getMethodLineCounts() {
+        return methodLineCounts;
     }
     
 
@@ -48,7 +56,7 @@ public class Counter {
                 if (!line.isEmpty() && !line.startsWith("//") && !line.startsWith("/*") && !line.startsWith("*")) {
                     totalLines++;
 
-                    if (line.startsWith("class")) {
+                    if (line.startsWith("public class")) {
                         classCount++;
                         if (methodLines > 0) {
                             classLines += methodLines;
@@ -58,7 +66,14 @@ public class Counter {
                     } else if (line.startsWith("public") || line.startsWith("private") || line.startsWith("protected")) {
                         methodCount++;
                         methodLines++;
+                        if (currentMethod != null) {
+                            methodLineCounts.put(currentMethod, methodLineCounts.getOrDefault(currentMethod, 0) + 1);
+                        }
+                        currentMethod = line.split("\\(")[0].split("\\s+")[1];
                     } else {
+                        if (currentMethod != null) {
+                            methodLineCounts.put(currentMethod, methodLineCounts.getOrDefault(currentMethod, 0) + 1);
+                        }
                         classLines++;
                     }
                 }
@@ -77,5 +92,11 @@ public class Counter {
         System.out.println("Total Methods: " + methodCount);
         System.out.println("Total Lines of Class Code: " + classLines);
         System.out.println("Total Lines of Method Code: " + methodLines);
+    }
+    public void printMethodLines() {
+        System.out.println("Method Lines:");
+        for (String method : methodLineCounts.keySet()) {
+            System.out.println(method + ": " + methodLineCounts.get(method));
+        }
     }
 }
